8968f35633a8fc6afb268bd3168f6dd8d089b6bf
ramakrishnamb <ramakrishnamb@gmail.com>
2020-06-09 23:26:14 +0530

    Add project files.

---
+++ SQLInterviewDatabaseProject/SQLInterview/ViewsBasics/ViewBasicsPrerequestiesQuery.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE tblEmployee
+(
+ Id int Primary Key,
+ Name nvarchar(30),
+ Salary int,
+ Gender nvarchar(10),
+ DepartmentId int
+)
+go
+CREATE TABLE tblDepartment
+(
+ DeptId int Primary Key,
+ DeptName nvarchar(20)
+)
+go
+Insert into tblDepartment values (1,'IT')
+Insert into tblDepartment values (2,'Payroll')
+Insert into tblDepartment values (3,'HR')
+Insert into tblDepartment values (4,'Admin')
+go
+Insert into tblEmployee values (1,'John', 5000, 'Male', 3)
+Insert into tblEmployee values (2,'Mike', 3400, 'Male', 2)
+Insert into tblEmployee values (3,'Pam', 6000, 'Female', 1)
+Insert into tblEmployee values (4,'Todd', 4800, 'Male', 4)
+Insert into tblEmployee values (5,'Sara', 3200, 'Female', 1)
+Insert into tblEmployee values (6,'Ben', 4800, 'Male', 3)
